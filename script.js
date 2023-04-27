bf7aa8024495b293a6fb3964630d36775cb3df4c
Marigza <marigola@mail.ru>
2023-04-27 21:50:40 +0300

    feat: create key illumination functions

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 {
   const body = document.querySelector('.body');
   let capsLock = false;
+
+  const keysCode = [
+    'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+    'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+    'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+    'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'
+  ];
   const keysEng = [
     '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
     'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
@@ -14,7 +22,7 @@
     'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
     'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#8657;', 'Shift',
     'Ctrl', 'Win', 'Alt', '_____', 'Alt', '&#8656;', '&#8659;', '&#8658;', 'Ctrl'
-  ]
+  ];
     //TODO сделать выбор между языками в зависимости от языка, записанного в локал сторадж
   
   let titleBlock = document.createElement('h1');
@@ -54,21 +62,48 @@
       keys[i].classList.add('key_extra-wide');
     }
   }
-  
+  // console.log(keys);
+  function lightingKeysByTap(event) {
+    let e = event.code;
+    let index = keysCode.indexOf(e);
+    let activeKey = keys.find(key => key.innerText === keysEng[index]);
+    keys.forEach(key => key.classList.remove('transition-down'));
+    activeKey.classList.add('transition-down');
+    activeKey.addEventListener('animationend', () => activeKey.classList.remove('transition-down'));
+    console.log(keysEng[index]);
+  };
 
+  function lightingKeysByClick(elem) {
+    keys.forEach(elem => elem.classList.remove('transition-down'));
+    if (elem.innerText === 'CapsLock') {
+      elem.classList.toggle('key_active');
+      elem.classList.contains('key_active') ? capsLock = true : capsLock = false;
+      console.log(capsLock);
+    } else {
+      elem.classList.add('transition-down');
+      console.log(elem.innerText);
+      elem.addEventListener('animationend', () => elem.classList.remove('transition-down'));
+    }
+    
+  }
+
+  //TODO объединить индексы массивов ключей и букв и вывести букву на экран в поле инпут
+
+  document.addEventListener('keydown', lightingKeysByTap);
+  keys.forEach(elem => elem.addEventListener('click', () => { lightingKeysByClick(elem) }))
 
   // function createCodeInd() {
   //   let arr = [];
   //   document.onkeydown = function (event) {
   //     //console.log(event)
-  //     arr.push(event.key)
+  //     arr.push(event.code)
   //     console.log(arr)
   //   }
   // } 
   //  createCodeInd()
-  // let arr1 = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'];
-  // let arr2 = ['tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'];
-  // let arr3 = ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'];
-  // let arr4 = ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift'];
-  // let arr5 = ['Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
+  // let arr1 = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'];
+  // let arr2 = ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'];
+  // let arr3 = ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'];
+  // let arr4 = ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'];
+  // let arr5 = ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 }
