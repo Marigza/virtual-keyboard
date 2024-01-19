28a7e742906edfc0ff5b3ff53adf4ad4c8ecb0fc
Marigza <98157246+Marigza@users.noreply.github.com>
2024-01-19 18:03:40 +0300

    Delete alert function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 {
-  alert ('Поведение кнопок при зажатом shift не реализовано')
   const body = document.querySelector('.body');
   const keysCode = [
     'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
